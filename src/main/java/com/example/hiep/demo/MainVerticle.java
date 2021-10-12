2e072a39e0e6a9f4be7652798ea499b580b6de64
Lê Hoàng Hiệp <hieplh@splus-software.com.vn>
2021-10-12 19:48:08 +0700

    Create connect to RabbitMQ

--- src/main/java/com/example/hiep/demo/MainVerticle.java
+++ src/main/java/com/example/hiep/demo/MainVerticle.java
@@ -1,14 +1,16 @@
 package com.example.hiep.demo;
 
-import com.example.hiep.demo.calculating.Client;
-import com.example.hiep.demo.calculating.Server;
+import com.example.hiep.demo.calculating.ClientExchangeRequest;
+import com.example.hiep.demo.calculating.ClientQueueRequest;
+import com.example.hiep.demo.calculating.ClientQueueResponse;
 import io.vertx.core.Vertx;
 
 public class MainVerticle {
 
     public static void main(String[] args) {
         Vertx vertx = Vertx.vertx();
-        vertx.deployVerticle(new Server());
-        vertx.deployVerticle(new Client());
+        vertx.deployVerticle(new ClientQueueRequest());
+        vertx.deployVerticle(new ClientExchangeRequest());
+        vertx.deployVerticle(new ClientQueueResponse());
     }
 }
